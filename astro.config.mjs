90c94cf1608fed5c46d57a5f38892929e9657e00
joechamul <88106215+joechamul@users.noreply.github.com>
2026-04-23 08:23:11 -0500

    fix: fix the build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 import { defineConfig } from 'astro/config';
-import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://joechamul.github.io',
   base: '/sewer-pipe-pros',
   trailingSlash: 'ignore',
-  integrations: [sitemap()],
-  build: {
-    format: 'directory',
-  },
 });
